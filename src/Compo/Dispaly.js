92382678da535b5cd4066d42688b3c4243641222
Shriyansh7870 <Shriyansh7870@gmail.com>
2023-09-16 13:51:39 +0530

    first commit

--- src/Compo/Dispaly.js
+++ src/Compo/Dispaly.js
@@ -1,20 +1,126 @@
-import React from "react";
-import Students from "./Students";
-import Contact from "./Contact";
+import { useState } from "react";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import Store from "./Store";
 import Home from "./Home";
-import { Routes, Route, Link } from "react-router-dom";
-export default function Dispaly() {
+import Contact from "./Contact";
+import Students from "./Students";
+import Edit from "./Edit";
+import Addstudent from "./AddStudent";
+const Display = () => {
+  const [StdData, setData] = useState([
+    {
+      Name: "John",
+      Age: 22,
+      Course: "Mern",
+      Batch: "October",
+      Change: "Edit",
+      id: 0,
+    },
+    {
+      Name: "Nancy",
+      Age: 24,
+      Course: "Mern",
+      Batch: "February",
+      Change: "Edit",
+      id: 1,
+    },
+    {
+      Name: "Priyanka",
+      Age: 22,
+      Course: "Mern",
+      Batch: "March",
+      Change: "Edit",
+      id: 2,
+    },
+    {
+      Name: "Shriyansh",
+      Age: 22,
+      Course: "Mern",
+      Batch: "Sepetember",
+      Change: "Edit",
+      id: 3,
+    },
+    {
+      Name: "Ranu",
+      Age: 22,
+      Course: "Mern",
+      Batch: "April",
+      Change: "Edit",
+      id: 4,
+    },
+    {
+      Name: "Priya",
+      Age: 22,
+      Course: "Mern",
+      Batch: "March",
+      Change: "Edit",
+      id: 5,
+    },
+    {
+      Name: "Raushan",
+      Age: 22,
+      Course: "Mern",
+      Batch: "March",
+      Change: "Edit",
+      id: 6,
+    },
+    {
+      Name: "Insha",
+      Age: 22,
+      Course: "Mern",
+      Batch: "March",
+      Change: "Edit",
+      id: 7,
+    },
+  ]);
   return (
-    <div className="Nav">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Students" element={<Students />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
+    <>
+      <BrowserRouter>
+        <div className="navbar">
+          <NavLink to="/" className="link">
+            Home
+          </NavLink>
+          <NavLink to="/Students" className="link">
+            Students
+          </NavLink>
+          <NavLink to="/Contact" className="link">
+            Contact
+          </NavLink>
+        </div>
 
-      <Link to="/">Home</Link>
-      <Link to="/Students">Students</Link>
-      <Link to="/Contact">Contact</Link>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/Students"
+            element={
+              <Store.Provider value={[StdData]}>
+                <Students />
+              </Store.Provider>
+            }
+          ></Route>
+
+          <Route path="/Contact" element={<Contact />}></Route>
+
+          <Route
+            path="/Edit"
+            element={
+              <Store.Provider value={[StdData]}>
+                <Edit />
+              </Store.Provider>
+            }
+          ></Route>
+
+          <Route
+            path="/Addstudent"
+            element={
+              <Store.Provider value={[StdData]}>
+                <Addstudent />
+              </Store.Provider>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
+export default Display;
--- src/Compo/Students.js
+++ src/Compo/Students.js
@@ -1,27 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const data = [
-  { name: "John", Age: 22, Course: "Mern", Batch: "October", Change: "Edit" },
-  { name: "Nancy", Age: 24, Course: "Mern", Batch: "February", Change: "Edit" },
-  { name: "Priyanka", Age: 22, Course: "Mern", Batch: "March", Change: "Edit" },
-  {
-    name: "Shriyansh",
-    Age: 22,
-    Course: "Mern",
-    Batch: "Sepetember",
-    Change: "Edit",
-  },
-  { name: "Ranu", Age: 22, Course: "Mern", Batch: "April", Change: "Edit" },
-  { name: "Priyanka", Age: 22, Course: "Mern", Batch: "March", Change: "Edit" },
-  { name: "Priyanka", Age: 22, Course: "Mern", Batch: "March", Change: "Edit" },
-  { name: "Priyanka", Age: 22, Course: "Mern", Batch: "March", Change: "Edit" },
-];
+import React, { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import Store from "./Store";
 
 export default function Students() {
+  const [Received] = useContext(Store);
+  const Navigate = useNavigate();
+  console.log(Received);
+
+  function Navi() {
+    Navigate("/AddStudent");
+  }
+
   return (
     <>
       <p className="Stname">Students Details</p>
-      <buton className="btn1">Add New Student</buton>
+      <button onClick={Navi} className="btn1">
+        Add New Student
+      </button>
       <table border={2} className="table">
         <thead>
           <tr>
@@ -33,16 +28,16 @@ export default function Students() {
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
+          {Received.map((item, index) => (
             <tr key={index}>
-              <td>{item.name}</td>
+              <td>{item.Name}</td>
               <td>{item.Age}</td>
               <td>{item.Course}</td>
               <td>{item.Batch}</td>
               <td>
-                <Link to={"/data/:" + item.name} state={data}>
-                  {item.Change}
-                </Link>
+                <NavLink to="/Edit" state={{ index }}>
+                  Edit
+                </NavLink>
               </td>
             </tr>
           ))}
